b0c963dfdc8b86ae851ae7690c585f2929761033
Ольга Гусева <o.peluhovskaya@bk.ru>
2022-09-14 19:20:29 +0300

    rework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,21 @@
+"use strict";
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+
+  let d = b ** 2 - 4 * a * c;
+  let newArr;
+  let firstRoot;
+  let secondRoot;
+
+if (d < 0) {
+  arr = [];
+} else if (d == 0) {
+  newArr = -b / (2 * a);
+  arr = [newArr];
+} else if (d > 0) {
+  firstRoot = (-b + Math.sqrt(d)) / (2 * a);
+  secondRoot = (-b - Math.sqrt(d)) / (2 * a);
+  arr = [firstRoot, secondRoot];
+}   
   return arr; // array
-}
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
 }
